ecb10c4fa197cf409cb81eaceba762751a8bd363
Hugo Serinese (RIT Student) <hhs2791@rit.edu>
2022-09-21 13:56:32 -0400

    api

--- src/server.js
+++ src/server.js
@@ -13,63 +13,68 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 // urlStruct[request.method], we recieve another object which
 // routes each individual url to a handler. We can index this
 // object in the same way we have used urlStruct before.
-const urlStruct = {
-  GET: {
-    '/': htmlHandler.getIndex,
-    '/style.css': htmlHandler.getCSS,
-    // '/success': jsonHandler.success,
-    // '/badRequest': jsonHandler.badRequest,
-    // '/unauthorized': jsonHandler.unauthorized,
-    // '/forbidden': jsonHandler.forbidden,
-    // '/internal': jsonHandler.interal,
-    // '/notImplemented': jsonHandler.notImplemented,
-    // notFound: jsonHandler.notFound,
-  },
-  HEAD: {
-    // '/getUsers': jsonHandler.getUsersMeta,
-    // notFound: jsonHandler.notFoundMeta,
-  },
+
+const parseBody = (request, response, handler) => {
+  
+  //The request will come in in pieces. We will store those pieces in this
+  //body array.
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+
+    handler(request, response, bodyParams);
+  });
+};
+
+const handlePost = (request, response, parsedUrl) => {
+  
+  //If they go to /addUser
+  if(parsedUrl.pathname === '/addUser') {
+    
+    //Call our below parseBody handler, and in turn pass in the
+    //jsonHandler.addUser function as the handler callback function.
+    parseBody(request, response, jsonHandler.addUser);
+  }
+};
+
+const handleGet = (request, response, parsedUrl) => {
+  //route to correct method based on url
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response);
+  } else {
+    htmlHandler.getIndex(request, response);
+  }
 };
 
 // function to handle requests
 const onRequest = (request, response) => {
-  // first we have to parse information from the url
+  //parse url into individual parts
+  //returns an object of url parts by name
   const parsedUrl = url.parse(request.url);
-  const params = query.parse(parsedUrl.query);
-  const acceptedTypes = request.headers.accept.split(',');
-  // console.log(request.url);
-  // console.log(urlStruct);
-  // switch(request.url){
-  //   case '/':
-  //     htmlHandler.getIndex(request,response);
-  //   break;
-  //   case '/style.css':
-  //     // htmlHandler.getCSS(request,response);
-  //   default:
-  //     htmlHandler.getIndex(request,response);
-  //   break;
-  // }
-  // next we need to ensure that we can handle the request
-  // method that they are making the request with. This server
-  // is only built to handle GET and HEAD requests, so we want
-  // to send back a 404 if they make anything else. We can use
-  // the HEAD version of notFound to send just a 404 status code.
 
-  if (!urlStruct[request.method]) {
-    return urlStruct.HEAD.notFound(request, response, acceptedTypes);
+  //check if method was POST, otherwise assume GET 
+  //for the sake of this example
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
   }
-
-  // now we check to see if we have something to handle the
-  // request. This syntax may look verbose, but essentially
-  // what we are doing is indexing into urlStruct by the method
-  // which returns another object. We then index into that object
-  // by the pathname to get the handler. Inside the if, we can
-  // use that same syntax to call the actual function.
-
-  if (urlStruct[request.method][parsedUrl.pathname]) {
-    return urlStruct[request.method][parsedUrl.pathname](request, response, acceptedTypes, params);
-  }
-  return urlStruct[request.method].notFound(request, response, acceptedTypes, params);
 };
 
 // start server
